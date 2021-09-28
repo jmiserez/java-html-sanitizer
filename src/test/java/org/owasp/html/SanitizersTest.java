b321f6b55ad4a1dac77769c52c771d6f153c667f
Jeremie Miserez <jeremie.miserez@bsi-software.com>
2021-09-28 15:05:14 +0200

    tel URIs: Add tests for missing RFC 3966 support (wrong escaping of separators)
    See https://datatracker.ietf.org/doc/html/rfc3966#section-3

--- src/test/java/org/owasp/html/SanitizersTest.java
+++ src/test/java/org/owasp/html/SanitizersTest.java
@@ -281,6 +281,78 @@ public class SanitizersTest extends TestCase {
     assertEquals(want, s.sanitize(input, new Context(context)));
   }
 
+  // note: currently fails due to escaping in org.owasp.html.HtmlStreamRenderer.writeOpenTag
+  @Test
+  public static final void testTelUriRfc3966WithAllowUrlProtocol(){
+    PolicyFactory s = new HtmlPolicyBuilder()
+            .allowElements("a")
+            .allowAttributes("href").onElements("a")
+            .allowStandardUrlProtocols()
+            .allowUrlProtocols("tel")
+            .toFactory();
+    String input = (
+            "<a href=\"tel:1234567890\">A local phone number</a>"
+                    + "<a href=\"tel:1-234-567\">A number with dashes</a>"
+                    + "<a href=\"tel:1.234.567\">A number with dots</a>"
+                    + "<a href=\"tel:023 456 78 90\">Spaces need not be escaped but can</a>"
+                    + "<a href=\"tel:(555)1234\">Brackets need not be escaped but can</a>"
+                    + "<a href=\"tel:+1234567890\">The leading plus is a separator and MUST NOT be escaped</a>"
+                    + "<a href=\"tel:tel:890;phone-context=+123-4-567\">The equals is a separator and MUST NOT be escaped but the plus in the parameter is not a separator and MUST be escaped</a>"
+    );
+    String want = (
+            "<a href=\"tel:1234567890\">A local phone number</a>"
+                    + "<a href=\"tel:1-234-567\">A number with dashes</a>"
+                    + "<a href=\"tel:1.234.567\">A number with dots</a>"
+                    + "<a href=\"tel:023%20456%2078%2090\">Spaces need not be escaped but can</a>"
+                    + "<a href=\"tel:%28555%291234\">Brackets need not be escaped but can</a>"
+                    // fails, see https://datatracker.ietf.org/doc/html/rfc3966#section-3
+                    + "<a href=\"tel:+1234567890\">The leading plus is a separator and MUST NOT be escaped</a>"
+                    // fails, see https://datatracker.ietf.org/doc/html/rfc3966#section-3
+                    + "<a href=\"tel:tel:890;phone-context=;&#43;123-4-567\">The equals is a separator and MUST NOT be escaped but the plus in the parameter is not a separator and MUST be escaped</a>");
+    assertEquals(want, s.sanitize(input));
+  }
+
+  // note: currently fails due to escaping in org.owasp.html.HtmlStreamRenderer.writeOpenTag
+  @Test
+  public static final void testTelUriRfc3966WithUrlClassifier(){
+    UrlContext context = new UrlContext(
+            new Absolutizer(UrlContext.DEFAULT.absolutizer.schemes,"https://foo.com/bar/"));
+    PolicyFactory s = new HtmlPolicyBuilder()
+            .allowElements("a")
+            .allowAttributes("href")
+            .matching(
+                    UrlClassifiers.builder()
+                            .scheme(BuiltinScheme.TEL)
+                            .schemeData(MediaTypeClassifiers.builder()
+                                            .type("text", "html")
+                                            .build())
+                            .build())
+            .onElements("a")
+            .toFactory();
+    String input = (
+            "<a href=\"tel:1234567890\">A local phone number</a>"
+                    + "<a href=\"tel:1-234-567\">A number with dashes</a>"
+                    + "<a href=\"tel:1.234.567\">A number with dots</a>"
+                    + "<a href=\"tel:023 456 78 90\">Spaces need not be escaped but can</a>"
+                    + "<a href=\"tel:(555)1234\">Brackets need not be escaped but can</a>"
+                    + "<a href=\"tel:+1234567890\">The leading plus is a separator and MUST NOT be escaped</a>"
+                    + "<a href=\"tel:tel:890;phone-context=+123-4-567\">The equals is a separator and MUST NOT be escaped but the plus in the parameter is not a separator and MUST be escaped</a>"
+    );
+    String want = (
+            "<a href=\"tel:1234567890\">A local phone number</a>"
+                    + "<a href=\"tel:1-234-567\">A number with dashes</a>"
+                    + "<a href=\"tel:1.234.567\">A number with dots</a>"
+                    // note: contrary to allowUrlProtocols, with UrlClassifier spaces are not escaped
+                    + "<a href=\"tel:023 456 78 90\">Spaces need not be escaped but can</a>"
+                    // note: contrary to allowUrlProtocols, with UrlClassifier brackets are not escaped
+                    + "<a href=\"tel:(555)1234\">Brackets need not be escaped but can</a>"
+                    // fails, see https://datatracker.ietf.org/doc/html/rfc3966#section-3
+                    + "<a href=\"tel:+1234567890\">The leading plus is a separator and MUST NOT be escaped</a>"
+                    // fails, see https://datatracker.ietf.org/doc/html/rfc3966#section-3
+                    + "<a href=\"tel:tel:890;phone-context=;&#43;123-4-567\">The equals is a separator and MUST NOT be escaped but the plus in the parameter is not a separator and MUST be escaped</a>");
+    assertEquals(want, s.sanitize(input, new Context(context)));
+  }
+
   @Test
   public static final void testIssue9StylesInTables() {
     String input = ""
